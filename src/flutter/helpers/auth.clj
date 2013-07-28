38821681f0b45d4470f96a7fedecd754f57f03fa
Shane Kilkelly <shanekilkelly@gmail.com>
2013-07-28 18:20:50 +0100

    whitespace/formatting changes

--- src/flutter/helpers/auth.clj
+++ src/flutter/helpers/auth.clj
@@ -1,18 +1,22 @@
 (ns flutter.helpers.auth
   (:require [noir.session :as session]))
 
+
 (defn logged-in? []
   (if (session/get :user-id)
     true
     false))
+
 
 (defn current-user []
   (if (logged-in?)
     (session/get :user-id)
     nil))
 
+
 (defn log-in [id]
   (session/put! :user-id id))
+
 
 (defn log-out []
   (session/clear!))
