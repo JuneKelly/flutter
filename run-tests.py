0bbada69fee551753e12d4eed8d21c7acc986516
Shane Kilkelly <shanekilkelly@gmail.com>
2013-08-06 22:06:50 +0100

    fix the script so that it does not leave the old server process hanging around

--- run-tests.py
+++ run-tests.py
@@ -1,8 +1,19 @@
-import os
 import subprocess
+import pexpect
+import time
 
-srv = subprocess.Popen(
-    ['lein', 'with-profile', 'test', 'ring', 'server-headless'],
-    stdout=subprocess.PIPE)
 
-spec = subprocess.call(['lein', 'with-profile', 'test', 'spec'])
+print '<>> Starting test server and running specs...'
+
+srv = pexpect.spawn('lein with-profile test trampoline ring server-headless')
+
+time.sleep(5)
+
+subprocess.call(' '.join(['lein', 'with-profile', 'test', 'spec']),
+                shell=True)
+
+print '<<>>'
+
+srv.terminate()
+srv.wait()
+print '<>> Test server stopped...'
